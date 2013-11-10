788b2a8113717cdc013d1b6fb0930197ee98f2dd
Alexander Lizin <sogimu@nxt.ru>
2013-11-10 21:28:07 +0700

    Added tasks Jake, Car, Html5. Added new sql dump

--- protected/guestBook.sql
+++ protected/guestBook.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.8
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 03, 2013 at 01:45 AM
--- Server version: 5.5.33a-MariaDB-log
--- PHP Version: 5.5.5
+-- Хост: 127.0.0.1
+-- Время создания: Ноя 10 2013 г., 15:27
+-- Версия сервера: 5.5.32
+-- Версия PHP: 5.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `guestBook`
+-- База данных: `guestbook`
 --
+CREATE DATABASE IF NOT EXISTS `guestbook` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `guestbook`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `messages`
+-- Структура таблицы `messages`
 --
 
 CREATE TABLE IF NOT EXISTS `messages` (
@@ -33,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `messages`
+-- Дамп данных таблицы `messages`
 --
 
 INSERT INTO `messages` (`id`, `text`) VALUES
@@ -44,18 +46,24 @@ INSERT INTO `messages` (`id`, `text`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tasks`
+-- Структура таблицы `tasks`
 --
 
 CREATE TABLE IF NOT EXISTS `tasks` (
   `id` int(4) NOT NULL AUTO_INCREMENT COMMENT 'task id',
   `taskIntoJSON` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'serialized array',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `tasks`
+-- Дамп данных таблицы `tasks`
 --
 
 INSERT INTO `tasks` (`id`, `taskIntoJSON`) VALUES
-(1, '{"items": [\r\n                {"id": 1, "pos": {"x": 0, "y": 0}, "path": "global/img/image0.png"},\r\n                {"id": 2, "pos": {"x": 1, "y": 0}, "path": "global/img/image1.png"},\r\n                {"id": 3, "pos": {"x": 0, "y": 1}, "path": "global/img/image2.png"},\r\n                {"id": 4, "pos": {"x": 1, "y": 1}, "path": "global/img/image3.png"}],\r\n                "row": 2,\r\n                "col": 2,\r\n            	"count": 4\r\n        }');
+(1, '{"items": [\r\n                {"id": 1, "pos": {"x": 0, "y": 0}, "path": "global/img/car/image0.png"},\r\n                {"id": 2, "pos": {"x": 1, "y": 0}, "path": "global/img/car/image1.png"},\r\n                {"id": 3, "pos": {"x": 0, "y": 1}, "path": "global/img/car/image2.png"},\r\n                {"id": 4, "pos": {"x": 1, "y": 1}, "path": "global/img/car/image3.png"}],\r\n                "row": 2,\r\n                "col": 2,\r\n            	"count": 4\r\n        }'),
+(2, '{"items": [\r\n                {"id": 1, "pos": {"x": 0, "y": 0}, "path": "global/img/html5/html5_swap_part1x1.png"},\r\n                {"id": 2, "pos": {"x": 0, "y": 1}, "path": "global/img/html5/html5_swap_part1x2.png"},\r\n                {"id": 3, "pos": {"x": 0, "y": 2}, "path": "global/img/html5/html5_swap_part1x3.png"},\r\n                {"id": 4, "pos": {"x": 0, "y": 3}, "path": "global/img/html5/html5_swap_part1x4.png"},\r\n                {"id": 1, "pos": {"x": 1, "y": 0}, "path": "global/img/html5/html5_swap_part2x1.png"},\r\n                {"id": 2, "pos": {"x": 1, "y": 1}, "path": "global/img/html5/html5_swap_part2x2.png"},\r\n                {"id": 3, "pos": {"x": 1, "y": 2}, "path": "global/img/html5/html5_swap_part2x3.png"},\r\n                {"id": 4, "pos": {"x": 1, "y": 3}, "path": "global/img/html5/html5_swap_part2x4.png"},\r\n                {"id": 1, "pos": {"x": 2, "y": 0}, "path": "global/img/html5/html5_swap_part3x1.png"},\r\n                {"id": 2, "pos": {"x": 2, "y": 1}, "path": "global/img/html5/html5_swap_part3x2.png"},\r\n                {"id": 3, "pos": {"x": 2, "y": 2}, "path": "global/img/html5/html5_swap_part3x3.png"},\r\n                {"id": 4, "pos": {"x": 2, "y": 3}, "path": "global/img/html5/html5_swap_part4x4.png"},\r\n                {"id": 1, "pos": {"x": 3, "y": 0}, "path": "global/img/html5/html5_swap_part4x1.png"},\r\n                {"id": 2, "pos": {"x": 3, "y": 1}, "path": "global/img/html5/html5_swap_part4x2.png"},\r\n                {"id": 3, "pos": {"x": 3, "y": 2}, "path": "global/img/html5/html5_swap_part4x3.png"},\r\n                {"id": 4, "pos": {"x": 3, "y": 3}, "path": "global/img/html5/html5_swap_part4x4.png"}],\r\n                "row": 4,\r\n                "col": 4,\r\n            	"count": 16\r\n        }'),
+(3, '{"items": [\r\n                {"id": 1, "pos": {"x": 0, "y": 0}, "path": "global/img/jake/jake_swap_part1x1.png"},\r\n                {"id": 2, "pos": {"x": 1, "y": 0}, "path": "global/img/jake/jake_swap_part1x2.png"},\r\n                {"id": 3, "pos": {"x": 0, "y": 1}, "path": "global/img/jake/jake_swap_part2x1.png"},\r\n                {"id": 4, "pos": {"x": 1, "y": 1}, "path": "global/img/jake/jake_swap_part2x2.png"}],\r\n                "row": 2,\r\n                "col": 2,\r\n            	"count": 4\r\n        }');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
